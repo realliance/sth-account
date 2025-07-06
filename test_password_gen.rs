14f801e771b578023c6b15afffc0103581746a8f
Christopher Miller <git@chrismiller.xyz>
2025-07-06 19:43:11 -0400

    clean up

--- test_password_gen.rs
+++
@@ -1,8 +0,0 @@
-// Test program to generate password hash for tests
-use sth_account::auth::Backend;
-
-#[tokio::main]
-async fn main() {
-    let hash = Backend::hash_password("password123").await.unwrap();
-    println!("Password hash for 'password123': {}", hash);
-}
